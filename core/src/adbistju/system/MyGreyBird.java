bbcce902127cc620a38480626cbf9945402c26ef
Adbistju <Adjytant@gmail.com>
2021-05-24 21:20:17 +0300

    les1

--- core/src/adbistju/system/MyGreyBird.java
+++ core/src/adbistju/system/MyGreyBird.java
@@ -8,19 +8,27 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyGreyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-
+	Texture imgBack;
+	float x = 10;
+	float y = 0;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		imgBack = new Texture("back.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(imgBack, 0, 0);
+		batch.draw(img, (x++)+(x++), y++);
+		if(x>1000){
+			x = 0;
+			y = 0;
+		}
 		batch.end();
 	}
 	
